f5c06d513ea408eab99023a4250381df1afa4f50
Eliana K <ekirshe@gmail.com>
2018-06-15 22:24:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,18 +22,18 @@ function dec(n){
   return n--;
 }
 
-function makeInt() {
+function makeInt(n) {
   parseInt('2', 10)
 }
 
-function makeInt() {
+function makeInt(n) {
   parseInt ('2.2222', 10)
 }
 
-function makeInt() {
+function makeInt(n) {
   parseInt('nonsense!', 10)
 }
 
-function preserveDecimal() {
+function preserveDecimal(n) {
   parseFloat('80.123999')
 }
